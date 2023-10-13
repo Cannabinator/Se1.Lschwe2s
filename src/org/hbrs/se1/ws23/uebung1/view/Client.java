d2923256f6b9d52d01a9fca33b729e1979d0cc89
Cannabinatoir <laurinschweers@gmail.com>
2023-10-13 18:29:08 +0200

    Finished Client.java

--- src/org/hbrs/se1/ws23/uebung1/view/Client.java
+++ src/org/hbrs/se1/ws23/uebung1/view/Client.java
@@ -3,20 +3,19 @@ package org.hbrs.se1.ws23.uebung1.view;
 
 
 import org.hbrs.se1.ws23.uebung1.control.Translator;
-import org.hbrs.se1.ws23.uebung1.control.GermanTranslator;
 import org.hbrs.se1.ws23.uebung1.control.objectFac;
 
 public class Client {
 
-		objectFac x1=new objectFac();
+		objectFac fac=new objectFac();
 		/*
 		 * Methode zur Ausgabe einer Zahl auf der Console
 		 * (auch bezeichnet als CLI, Terminal)
 		 *
 		 */
 		 void display( int aNumber ){
-			  Translator x2 = x1.createTransl();
-
+			  Translator x2 = fac.createTransl();
+			  String ret=x2.translateNumber(aNumber);
 			// In dieser Methode soll die Methode translateNumber
 			// mit dem übergegebenen Wert der Variable aNumber
 			// aufgerufen werden.
@@ -24,8 +23,7 @@ public class Client {
 			// Strenge Implementierung gegen das Interface Translator gewuenscht!
 
 
-			System.out.println("Das Ergebnis der Berechnung: " +
-					"[das Ergebnis an dieser Stelle]"  );
+			System.out.println("Das Ergebnis der Berechnung: " +ret);
 
 		 }
 }
