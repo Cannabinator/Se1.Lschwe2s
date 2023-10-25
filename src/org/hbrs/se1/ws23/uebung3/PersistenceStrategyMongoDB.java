8930b735f54100b0219dfdf9ca13015e7e02714c
Cannabinator <laurinschweers@gmail.com>
2023-10-25 14:28:15 +0200

    uebung 3 in meine Rep

---
+++ src/org/hbrs/se1/ws23/uebung3/PersistenceStrategyMongoDB.java
@@ -0,0 +1,28 @@
+package org.hbrs.se1.ws23.uebung3;
+
+import java.util.List;
+
+public class PersistenceStrategyMongoDB<E> implements PersistenceStrategy<E> {
+
+    //lol.java
+    @Override
+    public void openConnection() throws PersistenceException {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    @Override
+    public void closeConnection() throws PersistenceException {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    @Override
+    public void save(List<E> member) {
+        throw new UnsupportedOperationException("Not implemented!");
+
+    }
+
+    @Override
+    public List<E> load() {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+}
